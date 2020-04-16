c5ee76b4462001ecb6a8d6fa84c480703c51711f
yangtianci <yangtianci1@daojia.com.cn>
2020-04-16 19:00:01 +0800

    Initial commit

---
+++ app/src/main/java/com/aiyang/android_crashx/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aiyang.android_crashx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
